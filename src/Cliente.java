beecf9e00a9930d5ab2dd4e60a8c78e30e7be9cd
Letícia Queiroz <letciawanderleyletw@gmail.com>
2026-09-21 20:28:34 -0300

    feat: add near-final implementation of Client and Server classes

---
+++ src/Cliente.java
@@ -0,0 +1,34 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Cliente {
+    public static void main(String args[]) {
+        try (Socket conexao = new Socket("127.0.0.1", 5000);
+                DataOutputStream jogadaCliente = new DataOutputStream(conexao.getOutputStream());
+                DataInputStream jogadaServidor = new DataInputStream(conexao.getInputStream());
+                Scanner scanner = new Scanner(System.in);) {
+
+            System.out.println("Seja bem-vindo(a) ao Jogo - Pedra, Papel e Tesoura");
+            while (true) {
+                System.out.println("Digite a sua jogada: ");
+                String jogada = scanner.nextLine();
+                if(jogada.equalsIgnoreCase("SAIR")) {
+                    break;
+                }
+                jogada = jogada.toUpperCase();
+
+                // Request
+                jogadaCliente.writeUTF(jogada);
+                // Response
+                String resposta = jogadaServidor.readUTF();
+                System.out.println(resposta);
+            }
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Servidor.java
@@ -0,0 +1,67 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Random;
+
+public class Servidor {
+    public static void main(String args[]) {
+        try (ServerSocket servidor = new ServerSocket(5000)) {
+            try (Socket conexao = servidor.accept();
+                    DataInputStream entrada = new DataInputStream(conexao.getInputStream());
+                    DataOutputStream saida = new DataOutputStream(conexao.getOutputStream())) {
+
+                while (true) {
+
+                    String jogadaCliente = entrada.readUTF();
+
+                    String[] jogadas = { "PEDRA", "PAPEL", "TESOURA" };
+
+                    // gerar número aleatório e mapear para as possíveis jogadas
+                    Random random = new Random();
+                    int indiceAleatorio = random.nextInt(3);
+
+                    String jogadaServidor = jogadas[indiceAleatorio];
+                    String resultadoJogo = "inseriu uma jogada inválida.";
+
+                    if (jogadaServidor.equals(jogadaCliente)) {
+                        resultadoJogo = "empatou.";
+                    }
+
+                    else if (jogadaCliente.equals("PEDRA")) {
+                        if (jogadaServidor.equals("PAPEL"))
+                            resultadoJogo = "perdeu.";
+                        else if (jogadaServidor.equals("TESOURA"))
+                            resultadoJogo = "ganhou!";
+                    }
+
+                    else if (jogadaCliente.equals("TESOURA")) {
+                        if (jogadaServidor.equals("PAPEL"))
+                            resultadoJogo = "ganhou!";
+                        else if (jogadaServidor.equals("PEDRA"))
+                            resultadoJogo = "perdeu.";
+                    }
+
+                    else if (jogadaCliente.equals("PAPEL")) {
+                        if (jogadaServidor.equals("PEDRA"))
+                            resultadoJogo = "ganhou!";
+                        else if (jogadaServidor.equals("TESOURA"))
+                            resultadoJogo = "perdeu.";
+                    }
+
+                    String placar = "Servidor jogou " + jogadaServidor + ". Você " + resultadoJogo;
+
+                    saida.writeUTF(placar);
+                }
+
+            } catch (IOException e) {
+                System.out.println("Erro com o cliente: " + e.getMessage());
+                // e.printStackTrace();
+            }
+        } catch (IOException e) {
+            System.out.println("Não foi possível iniciar o servidor na porta 5000.");
+            e.printStackTrace();
+        }
+    }
+}
